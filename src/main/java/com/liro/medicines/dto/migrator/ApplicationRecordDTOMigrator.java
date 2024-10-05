60c10531279a46ecbc137ce9165b195cc76c1ac6
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-10-05 12:40:24 -0300

    Adding ApplicationRecordDTOMigrator

--- src/main/java/com/liro/medicines/dto/migrator/ApplicationRecordDTOMigrator.java
+++ src/main/java/com/liro/medicines/dto/migrator/ApplicationRecordDTOMigrator.java
@@ -1,4 +1,23 @@
 package com.liro.medicines.dto.migrator;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+@ToString
 public class ApplicationRecordDTOMigrator {
+    private Long animalId;
+    private Long medicineId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    private LocalDate applicationDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    private LocalDate endDate;
 }
