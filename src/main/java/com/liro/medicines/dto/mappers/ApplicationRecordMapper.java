72c899f1ee090241b5f328d82f79c1503282716d
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-10-21 18:51:23 -0300

    Adding MigrateApplicationRecord to mapper

--- src/main/java/com/liro/medicines/dto/mappers/ApplicationRecordMapper.java
+++ src/main/java/com/liro/medicines/dto/mappers/ApplicationRecordMapper.java
@@ -3,6 +3,7 @@ package com.liro.medicines.dto.mappers;
 
 import com.liro.medicines.dto.ApplicationRecordDTO;
 import com.liro.medicines.dto.BrandDTO;
+import com.liro.medicines.dto.migrator.ApplicationRecordDTOMigrator;
 import com.liro.medicines.dto.responses.ApplicationRecordResponse;
 import com.liro.medicines.dto.responses.BrandResponse;
 import com.liro.medicines.model.dbentities.ApplicationRecord;
@@ -17,4 +18,12 @@ public interface ApplicationRecordMapper {
 
     @Mapping(target = "medicine", ignore = true)
     ApplicationRecord applicationRecordDtoToApplicationRecord(ApplicationRecordDTO applicationRecordDTO);
+
+    @Mapping(target = "details", ignore = true)
+    @Mapping(target = "quantity", ignore = true)
+    @Mapping(target = "lote", ignore = true)
+    @Mapping(target = "controlNumber", ignore = true)
+    @Mapping(target = "serieNumber", ignore = true)
+    @Mapping(target = "medicine", ignore = true)
+    ApplicationRecord applicationRecordDTOMigratorToApplicationRecord(ApplicationRecordDTOMigrator applicationRecordDTOMigrator);
 }
